7758a40df537e4e82120b36cc7b9be457100fb82
DmDespert <ordimac@jim-dede.local>
2021-03-22 12:54:51 +0100

    feat: exo-2 terminé

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,57 @@
 // you can write js here
 console.log('exo-2');
+
+var week = "Déso, on est en semaine";
+var weekend = "Yep, sors ton barbecue";
+
+
+var date = new Date();
+var testActiveOrNot = false;
+var myDate = "";
+var myTime = "";
+var error = "Mauvais chiffre, entrez un chiffre entre 0 et 6";
+
+function isTestDay(trueOrNot) {
+    if(trueOrNot === true) {
+        myDate = prompt("Quel est le jour de la semaine ?");
+    }
+    else {
+        myDate = date.getDay();
+    }
+    return myDate;
+}
+
+function isTestHours(trueOrNot) {
+    if(trueOrNot === true) {
+        myTime = prompt("Quel heure est-il ?");
+    }
+    else {
+        myTime = date.getHours();
+    }
+    return myTime;
+}
+
+isTestDay(testActiveOrNot);
+isTestHours(testActiveOrNot);
+
+function messageFromDay(myDate, myTime) {
+    if(myDate == 0 || myDate == 6) {
+        return weekend;
+    }
+    else if(myDate == 5 && myTime > 16) {
+        return weekend;
+    }
+    else if (myDate == 1 && myTime < 9) {
+        return weekend;
+    }
+    else if(myDate > 6) {
+        return error;
+    }
+    else {
+        return week;
+    }
+}
+
+console.log("Il est " + myTime + "H");
+console.log("Nous sommes le " + myDate + "e jour de la semaine")
+console.log(messageFromDay(myDate, myTime));
