2e4f8290ebab32377cd48d8e4713c137b5c11ce0
DmDespert <ordimac@jim-dede.(none)>
2021-03-23 10:08:35 +0100

    feat: exo-5 terminé

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,24 @@
 // you can write js here
 
 console.log('exo-5');
+
+const input = "Hello world, i'm famous";
+const letters = ['a', 'e', 'i', 'o', 'u', 'y'];
+let resultArray = [];
+
+console.log(input.length);
+
+for(let i=0; i<input.length; i++) {
+    let letter;
+    letter = input.charAt(i);
+
+    for(let i=0; i<letters.length; i++) {
+        let compare = letters[i];
+        if(compare == letter) {
+            resultArray.push(letter);
+        }
+    }
+}
+
+console.log(resultArray.join(resultArray).toUpperCase());
+
